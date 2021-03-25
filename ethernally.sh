0dddcc976cbd73e03d0ab02ea75fddc918d1983d
Futur Fusionneur <7840350+fusionneur@users.noreply.github.com>
2021-03-25 20:01:46 +0200

    perl required

--- ethernally.sh
+++ ethernally.sh
@@ -2,6 +2,8 @@
 
 #this script automatically sets permanent adb via wifi and eventually start scrcpy through WiFi
 
+#requires perl to be installed
+
 #set the default adbd listening port
 port="5555"
 
